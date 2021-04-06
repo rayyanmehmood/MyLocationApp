5f28dfc3f73f4a44f0739734904f8f176d109859
rayyan mehmood <rayyan.khan9213@gmail.com>
2021-04-07 01:32:40 +0500

    this is my first commit

---
+++ app/src/main/java/com/example/currentlocation2/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.currentlocation2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView ttt2,ttt3;
+FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+        ttt2 = findViewById(R.id.tt2);
+        ttt3 = findViewById(R.id.tt3);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(
+
+                MainActivity.this
+        );
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    getCurrentlocation();
+                }
+            }
+        });
+    }
+        @SuppressLint("MissingPermission")
+        public void getCurrentlocation() {
+
+
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+
+                        Location location=task.getResult();
+                        if(location !=null){
+
+                            ttt2.setText(String.valueOf(location.getLatitude()));
+                            ttt3.setText(String.valueOf(location.getLongitude()));
+
+                        }
+
+                    }
+                });
+            }
+
+
+        }}
+
+
+
+
+
